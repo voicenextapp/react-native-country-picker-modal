89da54163e191b31c9021badbaa709a420daa8fb
koyu <me@koyu.space>
2024-12-18 15:28:23 +0100

    Works a little better now, but it still sucks

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,32 @@
-module.exports = {
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const {
+  resolver: { sourceExts, assetExts },
+} = getDefaultConfig(__dirname);
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
   resolver: {
-    sourceExts: ['js', 'json', 'jsx', 'ts', 'tsx']
-  }
-}
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
--- src/Flag.tsx
+++ src/Flag.tsx
@@ -2,7 +2,7 @@ import React, { memo } from 'react'
 import { Emoji } from './Emoji'
 import { CountryCode } from './types'
 import { useContext } from './CountryContext'
-import { useAsync } from 'react-async-hook'
+import { useAsync } from 'react-use'
 import {
   Image,
   StyleSheet,
@@ -55,7 +55,7 @@ const ImageFlag = memo(({ countryCode, flagSize }: FlagType) => {
         styles.imageFlag,
         { borderColor: 'transparent', height: flagSize },
       ]}
-      source={{ uri: asyncResult.result }}
+      source={{ uri: asyncResult.value }}
     />
   )
 })
@@ -72,7 +72,7 @@ const EmojiFlag = memo(({ countryCode, flagSize }: FlagType) => {
       style={[styles.emojiFlag, { fontSize: flagSize }]}
       allowFontScaling={false}
     >
-      <Emoji {...{ name: asyncResult.result! }} />
+      <Emoji {...{ name: asyncResult.value! }} />
     </Text>
   )
 })
